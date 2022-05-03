3df5cc31158338bfa7e1a01a432c39581b9de4b7
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-05-02 22:22:16 -0300

    init frame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "My egui App",
+        native_options,
+        Box::new(|cc| Box::new(Colorize::new(cc))),
+    );
+}
+
+#[derive(Default)]
+struct Colorize {}
+
+impl Colorize {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for Colorize {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Colorize");
+        });
+    }
 }
