876691b4f58d8cf44301936784409152eaf67ff8
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-05-07 18:39:04 -0300

    feat: list themes

--- commands/src/lib.rs
+++ commands/src/lib.rs
@@ -1,6 +1,10 @@
 pub mod kitty {
 
+    use std::env;
+    use std::path::PathBuf;
     use std::process::Command;
+
+    // use std::fs;
 
     fn kitty_theme_change(theme: &'static str) {
         let command = Command::new("kitty")
@@ -9,19 +13,38 @@ pub mod kitty {
             .arg("--reload-in=all")
             .arg(theme)
             .output()
-            .expect("failed to execute command");
+            .expect("failed to change kitty theme");
 
         let command_err = String::from_utf8_lossy(&command.stderr);
 
         if !command_err.is_empty() {
             panic!("{}", command_err);
         } else {
-            println!("Kitty theme changed successfully");
+            println!("kitty theme changed successfully");
+        }
+    }
+
+    fn kitty_theme_folder() {
+        const THEME_FOLDER: &'static str = "../../.config/kitty/themes/";
+        let command = Command::new("ls")
+            .arg(THEME_FOLDER)
+            .output()
+            .expect("failed to find kitty theme folder");
+
+        let command_err = String::from_utf8_lossy(&command.stderr);
+        let command_output = String::from_utf8_lossy(&command.stdout);
+
+        println!("{}", command_output);
+        if !command_err.is_empty() {
+            panic!("{}", command_err);
+        } else {
         }
     }
 
     pub fn execute() {
-        let theme = "Gruvbox Dark";
-        kitty_theme_change(theme);
+        // let theme = "Gruvbox Dark";
+        // let theme = "Dracula";
+        // kitty_theme_change(theme);
+        kitty_theme_folder();
     }
 }
