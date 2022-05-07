9828c09ba2029fd33d5baad89ec60c90b821ea81
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-05-07 04:51:59 -0300

    fix: remove target folder

--- commands/target/rls/debug/deps/commands-4ce5f3b8f63779b9.d
+++
@@ -1,5 +0,0 @@
-/Users/jonatanlima/dev/colorize/commands/target/rls/debug/deps/commands-4ce5f3b8f63779b9.rmeta: src/lib.rs
-
-/Users/jonatanlima/dev/colorize/commands/target/rls/debug/deps/commands-4ce5f3b8f63779b9.d: src/lib.rs
-
-src/lib.rs:
--- commands/target/rls/debug/deps/commands-caadd17652b37cfb.d
+++
@@ -1,5 +0,0 @@
-/Users/jonatanlima/dev/colorize/commands/target/rls/debug/deps/commands-caadd17652b37cfb.rmeta: src/lib.rs
-
-/Users/jonatanlima/dev/colorize/commands/target/rls/debug/deps/commands-caadd17652b37cfb.d: src/lib.rs
-
-src/lib.rs:
