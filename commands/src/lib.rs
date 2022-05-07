52521ccabd99c013f8f38a8c8bc64f0c05d5e8e0
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-05-07 05:28:12 -0300

    feat: add kitty change theme color

--- commands/src/lib.rs
+++ commands/src/lib.rs
@@ -1,7 +1,20 @@
+use std::process::Command;
+
 pub struct Commands {}
 
 impl Commands {
     pub fn execute() {
-        println!("Execute");
+        let list_dir = Command::new("kitty")
+            .arg("+kitten")
+            .arg("themes")
+            .arg("--reload-in=all")
+            .arg("Ayu")
+            .output()
+            .expect("failed to execute process");
+
+        let list_dir_output = String::from_utf8_lossy(&list_dir.stdout);
+
+        println!("list_dir status: {}", &list_dir.status);
+        println!("list_dir outupt: {}", list_dir_output);
     }
 }
