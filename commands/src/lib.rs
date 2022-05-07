0dce39f474542f126b05067f261274833a016a39
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-05-07 18:54:33 -0300

    feat: we don't need external lib to list files in a folders

--- commands/src/lib.rs
+++ commands/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod kitty {
 
-    use std::env;
-    use std::path::PathBuf;
     use std::process::Command;
 
-    // use std::fs;
+    fn convert_to_string(command: &std::vec::Vec<u8>) -> String {
+        return String::from_utf8_lossy(command).to_string();
+    }
 
     fn kitty_theme_change(theme: &'static str) {
         let command = Command::new("kitty")
@@ -15,7 +15,7 @@ pub mod kitty {
             .output()
             .expect("failed to change kitty theme");
 
-        let command_err = String::from_utf8_lossy(&command.stderr);
+        let command_err = convert_to_string(&command.stderr);
 
         if !command_err.is_empty() {
             panic!("{}", command_err);
@@ -25,19 +25,22 @@ pub mod kitty {
     }
 
     fn kitty_theme_folder() {
+        // TODO: Move THEME_FOLDER to env
         const THEME_FOLDER: &'static str = "../../.config/kitty/themes/";
         let command = Command::new("ls")
             .arg(THEME_FOLDER)
             .output()
             .expect("failed to find kitty theme folder");
 
-        let command_err = String::from_utf8_lossy(&command.stderr);
-        let command_output = String::from_utf8_lossy(&command.stdout);
+        let command_err = convert_to_string(&command.stderr);
+        let command_output = convert_to_string(&command.stdout);
 
-        println!("{}", command_output);
         if !command_err.is_empty() {
             panic!("{}", command_err);
         } else {
+            let theme_split = command_output.split(".conf\n");
+            let vec: Vec<&str> = theme_split.collect();
+            println!("{:?}", vec);
         }
     }
 
