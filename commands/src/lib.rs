53a9a01fc0a9c98a1422ab9054038afef4a686da
Jonatan Lima (balu) <jotanlima@gmail.com>
2022-05-08 17:52:45 -0300

    feat: add capitalize method

--- commands/src/lib.rs
+++ commands/src/lib.rs
@@ -3,6 +3,7 @@ pub mod kitty {
     use dotenv::dotenv;
     use std::env;
     use std::process::Command;
+    use std::vec::Vec;
 
     fn convert_to_string(command: &std::vec::Vec<u8>) -> String {
         return String::from_utf8_lossy(command).to_string();
@@ -26,6 +27,22 @@ pub mod kitty {
         }
     }
 
+    fn capitalize(s: &str) -> String {
+        s[0..1].to_uppercase() + &s[1..]
+    }
+
+    fn first<T>(v: &Vec<T>) -> Option<&T> {
+        v.first()
+    }
+
+    fn capitalize_letters(s: &str) -> String {
+        let letter_split_first = s.split("_");
+        let letter_split_last = s.split("_").last().unwrap();
+        let letter = capitalize(letter_split_last);
+
+        return format!("{}", letter);
+    }
+
     fn kitty_theme_folder() {
         dotenv().ok();
         let theme_folder = env::var("THEME_FOLDER_URL").expect("must be set");
@@ -41,11 +58,21 @@ pub mod kitty {
             panic!("{}", command_err);
         } else {
             let theme_split = command_output.split(".conf\n");
-            let vec: Vec<&str> = theme_split.collect();
-            println!("{:?}", vec);
+            let mut vec_split: Vec<&str> = theme_split.collect();
+
+            vec_split.pop();
+
+            let mut vec_theme_sanitized = vec![];
+            for theme_name in vec_split {
+                // let mut theme_name_sanitized = capitalize_letters(theme_name);
+                let mut theme_name_sanitized = capitalize(theme_name);
+                theme_name_sanitized = theme_name_sanitized.replace("_", " ");
+                vec_theme_sanitized.push(theme_name_sanitized);
+            }
+            println!("{:?}", vec_theme_sanitized);
 
             // To each theme name uppercase first letter and empty string
-            todo!();
+            // todo!();
         }
     }
 
